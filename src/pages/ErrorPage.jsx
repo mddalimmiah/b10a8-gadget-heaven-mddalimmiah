a5ebef131e52ebc0b78883508b9e0261e4806c52
mddalimmiah <mddalimsaif@gmail.com>
2025-02-09 23:14:39 +0600

    added path

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2 className='text-center text-4xl'>Page not Found</h2>
+            <p className='text-center'>404</p>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayouts from "../layouts/MainLayouts";
+import ErrorPage from "../pages/ErrorPage";
+import Home from "../pages/Home";
+import Dashboard from "../pages/Dashboard";
+import Statistics from "../pages/Statistics";
 const routes =createBrowserRouter([
 
     {
       path:'/',
       element: <MainLayouts></MainLayouts>,
+      errorElement: <ErrorPage></ErrorPage>,
+      children:[
+        {
+          path:'/',
+          element: <Home></Home>,
+        },
+        {
+          path:'/dashboard',
+          element: <Dashboard></Dashboard>,
+        },
+        {
+          path:'/statistics',
+          element: <Statistics></Statistics>,
+        },
+      ],
+      
     },
   ])
 
